6c51abeaf593aa1b1f26da07fd769dd7d3ad9de5
cristal <kobzarvllv@gmail.com>
2024-04-06 16:44:22 +0300

    test to check the buttons

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_search_for_the_add_to_cart_button(browser):
     browser.get(link)
-    time.sleep(10)
+    browser.implicitly_wait(5)
     button = browser.find_element(
         By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
 
